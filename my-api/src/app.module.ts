8528e8860c9eee0439873a72a38d70be5cc28cef
kchang4 <2335501+kchang4@users.noreply.github.com>
2023-08-27 03:44:38 +0000

    added mongoose

--- my-api/src/app.module.ts
+++ my-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MealModule } from './meal/meal.module';
 
 @Module({
-  imports: [],
+  imports: [MealModule],
   controllers: [AppController],
   providers: [AppService],
 })
